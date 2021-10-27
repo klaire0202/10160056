f32c7247813fe943af8979288d9a1731d750712f
klaire0202 <91170773+klaire0202@users.noreply.github.com>
2021-10-27 11:47:46 +0800

    Add files via upload

---
+++ week_07/10160056_week07-5.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    for(i=0;i<=3;i++)
+    {
+        printf("有幾次呢?\n");
+    }
+    for(int i=0;i<3;i++)
+    {
+        printf("老師建議的寫法,有幾次?\n");
+    }
+}
