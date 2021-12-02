6d264e73b6cd48461277123a1e7ee86f6741a606
klaire0202 <irene20030202@gmail.com>
2021-12-02 20:55:32 +0800

    這是我第一次用git指令commit

--- week3-4.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a,b;
-    printf( "Hello World\n");
-    printf( "Hello%dWorld\n", 300);
-    printf( "%d %d     %d", 300, 200, 100 );
-}
---
+++ week_03/week3-3.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf( "Hello World\n");
+    printf( "%d", 300 );
+}
+
